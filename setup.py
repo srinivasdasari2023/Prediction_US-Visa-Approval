cc2d219d509f2ec183d286b99d4e2e04a7380a35
srinivasDasari <srinivasdasari2023@gmail.com>
2024-07-13 21:20:34 +0530

    Final updates

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Srinivasdasari2023",
+    author_email="srinivasdasari2023gmail.com",
+    packages=find_packages()
+)
